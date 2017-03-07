b6e7172e142ed06fb20aa461bc6cae74c9ec1fb7
zubeer95 <progetti.zubeer@gmail.com>
2017-03-07 23:59:35 +0100

    Update zobeer2

---
+++ GoldRush/src/ZobeerMohammad2.java
@@ -0,0 +1,25 @@
+package goldrush;
+
+import goldrush.GoldDigger;
+
+/*
+ * Code used in the "Software Engineering" course.
+ *
+ * Copyright 2017 by Claudio Cusano (claudio.cusano@unipv.it)
+ * Dept of Electrical, Computer and Biomedical Engineering,
+ * University of Pavia.
+ */
+
+/**
+ *
+ * @author Zubeer
+ */
+public class ZobeerMohammad2  extends GoldDigger{
+
+    @Override
+    public int chooseDiggingSite(int[] distances) {
+    return 4;
+
+    }
+    
+}
