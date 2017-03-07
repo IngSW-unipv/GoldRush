aa66a69c8d8e19b21f20a51db8abea53c380f7e5
lorenzo <lorenzo@BonatiHome>
2017-03-07 22:55:26 +0100

    ciao

---
+++ GoldRush/src/goldrush/ALolloSan.java
@@ -0,0 +1,24 @@
+/*
+ * Code used in the "Software Engineering" course.
+ *
+ * Copyright 2017 by Claudio Cusano (claudio.cusano@unipv.it)
+ * Dept of Electrical, Computer and Biomedical Engineering,
+ * University of Pavia.
+ */
+package goldrush;
+
+/**
+ *
+ * @author 
+ */
+public class ALolloSan extends GoldDigger{
+
+    @Override
+    public int chooseDiggingSite(int[] distances) {
+        return 1;
+    }
+    
+
+    
+    
+}
