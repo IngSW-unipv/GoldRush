2074a2ccc9fd3cab1d9b333cdabdd6400bff5eca
cl417911 <cl417911@aga20>
2017-03-07 16:06:02 +0100

    random 2 tentativo

--- GoldRush/src/goldrush/BergamaschiMartino.java
+++ GoldRush/src/goldrush/BergamaschiMartino.java
@@ -9,6 +9,7 @@ package goldrush;
 
 import java.util.Random;
 
+
 /**
  *
  * @author cl417911
@@ -27,7 +28,23 @@ public class BergamaschiMartino extends GoldDigger {
     @Override
     public int chooseDiggingSite(int[] distances) {
         // Each site has the same chance of being chosen independently on its distance.
-        return randomGenerator.nextInt(distances.length);
+        return randomChoice();
     }
     
+    private int randomChoice() {
+        
+        double value = Math.random();
+        if(value < 0.08)
+            return 0;
+        else if(value < 0.34)
+            return 1;
+        else if(value < 0.52)
+            return 2;
+        else if(value < 0.66)
+            return 3;
+        else if(value < 0.92)
+            return 4;
+        else
+            return 5;
+    }
 }
