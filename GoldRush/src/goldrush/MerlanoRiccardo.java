537c69bb90d9625501e6c700b8c2606a975388f7
cl427720 <cl427720@aga24>
2017-03-07 15:32:11 +0100

    creazione minatore + strategia semplice di visitare solo i 3 siti più vicini

---
+++ GoldRush/src/goldrush/MerlanoRiccardo.java
@@ -0,0 +1,34 @@
+/*
+ * Code used in the "Software Engineering" course.
+ *
+ * Copyright 2017 by Claudio Cusano (claudio.cusano@unipv.it)
+ * Dept of Electrical, Computer and Biomedical Engineering,
+ * University of Pavia.
+ */
+package goldrush;
+
+/**
+ *
+ * @author Merlano Riccardo 427720
+ */
+public class MerlanoRiccardo extends GoldDigger {
+    
+    int sito=0, i=0;
+
+    @Override
+    public int chooseDiggingSite(int[] distances) {
+        
+        if(!(i>2))
+            sito=i;
+        else
+        {
+            i=0;
+            sito=i;
+        }
+        
+        i++;
+        
+        return sito;
+    }
+    
+}
