eb93a33c92a7df73dd31556648e92627f5ed5497
cl427358 <cl427358@aga33>
2017-03-07 16:10:01 +0100

    Primo commit

---
+++ GoldRush/src/goldrush/DumbDigger.java
@@ -0,0 +1,26 @@
+/*
+ * Code used in the "Software Engineering" course.
+ *
+ * Copyright 2017 by Claudio Cusano (claudio.cusano@unipv.it)
+ * Dept of Electrical, Computer and Biomedical Engineering,
+ * University of Pavia.
+ */
+package goldrush;
+
+/**
+ *
+ * @author cl427358
+ */
+public class DumbDigger extends GoldDigger{
+    int days;
+    
+    DumbDigger() {
+        days = 0;
+    }
+
+    @Override
+    public int chooseDiggingSite(int[] distances) {
+        return 1;
+    }
+    
+}
