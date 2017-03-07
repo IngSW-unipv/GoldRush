cc7e7d8cb5774b96894d0adf0daf846daec45ddf
Elisa <Elisa@A230474-PC>
2017-03-07 16:41:20 +0100

    Adding a new player

--- GoldRush/src/goldrush/ChiapponiElisa.java
+++ GoldRush/src/goldrush/ChiapponiElisa.java
@@ -11,22 +11,57 @@ import static goldrush.GoldRush.DEFAULT_DAYS;
 
 /**
  *
- * @author Elisa Chiapponi(426982), 
+ * @author Elisa Chiapponi(426982),
  */
-public class ChiapponiElisa extends GoldDigger{
+public class ChiapponiElisa extends GoldDigger {
 
     private int choosenSite;
-        
+    private int[] bestSites;
+    private static int day = 0;
 
-    public ChiapponiElisa(){
-        this.choosenSite=0;
-        
+    public ChiapponiElisa() {
+        this.choosenSite = 0;
+        this.bestSites=new int[DEFAULT_DAYS];
     }
-    
+
     @Override
     public int chooseDiggingSite(int[] distances) {
         return choosenSite;
     }
+
+    @Override
+    public void dailyOutcome(int revenue, int[] distances, int[] diggers) {
+
+        
+        int digSite = 0;
+        float guadagno = 0;
+        float guadTmp;       
+        
+        
+        for (int i = 0; i < diggers.length; i++) {
+            guadTmp = 5 * (12 - 2 * distances[i]);
+            if (diggers[i] != 0) {
+                guadTmp = guadTmp / diggers[i];
+            }
+
+            if (guadTmp > guadagno) {
+                guadagno=guadTmp;
+                if(digSite==0){
+                    digSite=1;
+                }else{
+                    digSite = i-1;
+                }
+                
+            }
+        }
+        
+        //bestSites[day]=digSite;
+        setChoosenSite(digSite);
+        //day++;
+    }
     
     
+    public void setChoosenSite(int site) {
+        this.choosenSite = site;
+    }
 }
