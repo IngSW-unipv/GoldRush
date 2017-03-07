01629386a994ad3433803abb8cd0d76b3d40da15
cl418743 <cl418743@aga36>
2017-03-07 15:37:25 +0100

    prima strategia Rogledi-Riccardi

---
+++ GoldRush/src/goldrush/RoglediRiccardi.java
@@ -0,0 +1,34 @@
+/*
+ * Code used in the "Software Engineering" course.
+ *
+ * Copyright 2017 by Claudio Cusano (claudio.cusano@unipv.it)
+ * Dept of Electrical, Computer and Biomedical Engineering,
+ * University of Pavia.
+ */
+package goldrush;
+
+/**
+ *
+ * @author cl418743
+ */
+public class RoglediRiccardi extends GoldDigger{
+    private String namesurname;     //Rogledi 418743 Riccardi 418760
+    
+
+    @Override
+    public int chooseDiggingSite(int[] distances) {
+        int i;
+        int x=0;
+        int ris=0;
+        for(i=0; i< distances.length; i++){
+            if(x>distances[i]){
+                x=distances[i];
+                ris=i;
+            }
+        }
+        return ris;
+    }
+    
+   
+    
+}
