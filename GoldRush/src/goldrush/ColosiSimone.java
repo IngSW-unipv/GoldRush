65e49e96e6d57344bd3134e1ddf394459ac17a3e
Simone <Simone@10.87.130.83>
2017-03-07 22:02:31 +0100

    PrimoPassoVerssoLaLuna

--- GoldRush/src/goldrush/ColosiSimone.java
+++ GoldRush/src/goldrush/ColosiSimone.java
@@ -7,6 +7,8 @@
  */
 package goldrush;
 
+import static java.lang.Math.abs;
+
 /**
  *
  * @author Simone
@@ -17,9 +19,10 @@ public class ColosiSimone extends GoldDigger{
     protected int[] distances;
     protected int[] diggers;
     protected int posizione=3;
+    protected final int[] tempi={30,140,200,240,280,310};
     
     public ColosiSimone(){
-        revenue=0;
+        revenue=3;
         distances=new int[6];
         diggers=new int[6];
     }
@@ -36,18 +39,49 @@ public class ColosiSimone extends GoldDigger{
         this.revenue=revenue;
         this.distances=distances.clone();
         this.diggers=diggers.clone();
-        
-        int i=0;
-        int max=0;
-        int count=0;
-                
-        for(Integer c:diggers){
-            if(c>max){
-                max=c;
-                count=i;
-            }
-            i++;
-        }
-        posizione=count;
+   
+//        int i=0;
+//        int max=0;
+//        int count=0;
+//                
+//        for(Integer c:diggers){
+//            if(c>max){
+//                max=c;
+//                count=i;
+//            }
+//            i++;
+//        }
+//        posizione=count;
+       
+       double[] g;
+       g=new double[6];
+       for(int i=0;i<diggers.length;i++)
+         g[i]=5*((12-(double)tempi[i]*(2/60))/(double)diggers[i]);
+       
+       double media=0;
+       for(Double c:g)
+           media+=c;
+       
+       media=media/6;
+       
+       double[] error=new double[6];       
+       for(int i=0;i<g.length;i++){
+           error[i]=abs(g[i]-media);
+            if(g[i]==0){
+                posizione=i;
+            }    
+       }
+       
+       int tmp=0;
+       for(int i=0;i<error.length;i++)
+           if(i==0)
+               tmp=(int)error[i];
+           else    
+                if((int)error[i]<tmp)
+                    tmp=(int)error[i];
+       
+       for(int i=5;i<=0;i--)
+           if((int)error[i]==tmp)
+               posizione=i;
     }
 }
