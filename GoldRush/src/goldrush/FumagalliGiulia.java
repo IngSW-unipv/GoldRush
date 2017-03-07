19d3d716bafb098e8623ec098f83f77d7272d2b1
cl426792 <cl426792@aga18>
2017-03-07 16:46:32 +0100

    versione 2

--- GoldRush/src/goldrush/FumagalliGiulia.java
+++ GoldRush/src/goldrush/FumagalliGiulia.java
@@ -15,20 +15,30 @@ import java.util.Random;
  */
 public class FumagalliGiulia extends GoldDigger {
     int place, days, diggers[];
+   // int diggers;
+  //  Random randomGenerator;
     
     FumagalliGiulia(){
+    //    randomGenerator = new Random();
         days = 0;
     }
     
     @Override
     public void dailyOutcome(int revenue, int[] distances, int[] diggers) { 
         this.diggers = diggers;
-        System.out.println(place + " - " + diggers[place]);
     }
     
     @Override
     public int chooseDiggingSite(int[] distances){
+        int tot = 0, c;
         if(days > 0){
+    //        if(diggers > 1){
+    //            place = randomGenerator.nextInt(3);
+    //        }
+            for(int i = 0; i < distances.length; i ++){
+                tot += diggers[i];
+            }
+            c = tot/4;
             for(int i = distances.length - 1; i >= 0 ; i --){
                 if(diggers[i]<5){
                     place = i;
